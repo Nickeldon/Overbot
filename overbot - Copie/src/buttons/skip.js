e42524f8dadaa2a826d275f46d79463376e53c3e
Nickeldon <50726216+Nickeldon@users.noreply.github.com>
2023-12-16 14:44:01 -0500

    Delete non-necessary folder

--- overbot - Copie/src/buttons/skip.js
+++
@@ -1,7 +0,0 @@
-module.exports = async ({  inter, queue }) => { 
-    if (!queue) return inter.reply({ content: `No music currently playing... try again ? ❌`, ephemeral: true });
-    
-    const success = queue.node.skip();
-
-    return inter.reply({ content: success ? `Current music ${queue.currentTrack} skipped ✅` : `Something went wrong ${inter.member}... try again ? ❌`, ephemeral: true});
-}
